0da4b2f3807a3aceafc9e51afaa03495f9a00ac2
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2025-07-30 17:11:03 +0100

    fixes: changed validate magic link url

--- src/main/java/com/backend/service/auth/AuthServiceImpl.java
+++ src/main/java/com/backend/service/auth/AuthServiceImpl.java
@@ -177,7 +177,7 @@ public class AuthServiceImpl implements AuthService {
         magicLink.setExpiresAt(Instant.now().plusMillis(magicLinkExpiration));
         magicLinkRepository.save(magicLink);
 
-        String link = String.format("%s/auth/magic-link/validatelink\n=%s", frontendUrl, token);
+        String link = String.format("%s/auth/magic-link/validatelink?magic-link\n=%s", frontendUrl, token);
         emailService.sendMagicLink(email, link);
         log.info("Magic link sent to {} with token: {}", email, token);
     }
