f4080ef362a3d663ca8c80c8f5e41e7ca927f12a
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2025-08-07 14:05:14 +0100

    feature: filtered request based on host port

--- src/main/java/com/backend/security/profile/HostBasedProfileFilter.java
+++ src/main/java/com/backend/security/profile/HostBasedProfileFilter.java
@@ -4,19 +4,23 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class HostBasedProfileFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        log.info("------------------------------------------------------------------------------------ PORT DETECTED --------------------------------------------------");
         String host = request.getServerName();
         int port = request.getServerPort();
         String fullHost = host + ":" + port;
+        log.info("Routing request from port to application properties {} " , fullHost);
 
         switch (fullHost) {
             case "localhost:3000" -> System.setProperty("spring.profiles.active", "dev");
