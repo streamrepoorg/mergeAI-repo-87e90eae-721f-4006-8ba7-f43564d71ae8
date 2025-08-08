c4ebf2b00117bcd0bdc9be86c9b5ccf3cd230596
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2025-08-08 09:29:48 +0100

    fixes: Stop tracking security and config files

--- src/main/java/com/backend/service/github/GithubService.java
+++
@@ -1,8 +0,0 @@
-package com.backend.service.github;
-
-import com.backend.dto.request.GithubStatusResponse;
-
-public interface GithubService {
-    String processRepository(String githubLink);
-    GithubStatusResponse getRepositoryStatus(String repositoryId);
-}
